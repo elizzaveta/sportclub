d29809b5c55e5ea6e46070ccda951be1d2559b79
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2022-01-08 21:46:20 +0200

    implemented an example of sending a token in header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/shared/common/Header/Header";
 import Footer from "./components/shared/common/Footer/Footer";
 import Landing from "./components/shared/landing/Landing";
 import Subscription from "./components/shared/subscription/Subscription";
-import React, { useState } from 'react'
+import React from 'react'
 import {
     BrowserRouter,
     Routes,
@@ -13,13 +13,20 @@ import PersonalSubscription from "./components/user/personalSubscription/Persona
 import EditSubscription from "./components/administration/subscription/EditSubscription";
 import AdministrationHome from "./components/administration/home/AdministrationHome";
 import Login from "./components/login/Login";
+import useToken from "./useToken";
+
+function clearToken() {
+    localStorage.removeItem('token');
+}
 
 function App() {
-    const [token, setToken] = useState();
+    const { token, setToken } = useToken();
 
-    if(!token) {
-        return <Login setToken={setToken} />
-    }
+    //if(!token) {
+     //   return <Login setToken={setToken} />
+    //}
+
+    // rendering the login page instead of
 
     return (
         <BrowserRouter>
@@ -27,10 +34,10 @@ function App() {
             <Routes>
                 <Route path="/" element={<Landing/>}/>
                 <Route path="/subscription" element={<Subscription/>}/>
-                <Route path="/my-subscription" element={<PersonalSubscription/>}/>
-                <Route path="/administration/edit-subscription" element={<EditSubscription/>}/>
-                <Route path="/administration" element={<AdministrationHome/>}/>
-                <Route path="/login" element={<Login/>}/>
+                <Route path="/my-subscription" element={token ? <PersonalSubscription token={token}/> : <Login setToken={setToken}/>}/>
+                <Route path="/administration/edit-subscription" element={token ? <EditSubscription/> : <Login setToken={setToken}/>}/>
+                <Route path="/administration" element={token ? <AdministrationHome/> : <Login setToken={setToken}/>}/>
+                <Route path="/login" element={<Login setToken={setToken}/>}/>
             </Routes>
             <Footer/>
         </BrowserRouter>
