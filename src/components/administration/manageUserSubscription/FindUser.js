76babe22734450b6cc1f7e88cd1ff36a66b6e33a
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2022-01-09 18:03:55 +0200

    added qr scanner

--- src/components/administration/manageUserSubscription/FindUser.js
+++ src/components/administration/manageUserSubscription/FindUser.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react";
 import {API} from "../../../index";
 import {Navigate, useNavigate} from 'react-router-dom';
 import ManageUser from "./ManageUser";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
 
 
 
@@ -15,6 +16,16 @@ export default function FindUser() {
             <form onSubmit={()=>navigate("/administration/manage-user/"+userId)}>
                 <label>
                     <label>Enter or scan user card id:</label>
+                    <BarcodeScannerComponent
+                        width={500}
+                        height={500}
+                        onUpdate={(err, result) => {
+                        if (result) {
+                            setUserId(result.text);
+                            navigate(`/administration/manage-user/${result.text}`)
+                        }
+                        else { /* TODO */ }
+                    }}/>
                     <input  className="login-input"  onChange={e => setUserId(e.target.value)} />
                 </label>
                 <div>
